9ad1c31ad60aba6bb8ea6a85c3c0a02547ac41eb
Jack Mason <jack.mason1@gmail.com>
2021-04-20 14:40:31 -0400

    Schema and seeder updated. Some 4 to 2 space edits.

--- server/db/Seeder.js
+++ server/db/Seeder.js
@@ -10,18 +10,28 @@ class Seeder {
   static async seed() {
     let loadedData = fs.readFileSync('data.json');
     let contents = JSON.parse(loadedData);
-    
+
     //console.log(contents.data.petTypes[0]);
     let petTypes = contents.data.petTypes;
+    let adoptablePets = contents.data.adoptablePets;
+
     try{
       petTypes.forEach(async (petType) => {
           let query = 'INSERT INTO pet_types (type, img_url, description) VALUES ($1, $2, $3)';
           let result = await pool.query(query, [petType.type, petType.imgURL, petType.description]);
-      });
-      pool.end();
+        });
+
+      adoptablePets.forEach( async (pet) => {
+        let idArr = await pool.query('INSERT INTO adoptable_pets (name, img_url, age, vaccination_status, adoption_story, available_for_adoption, pet_type_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id', [pet.name, pet.img_url, pet.age, pet.vaccination_status, pet.adoption_story, pet.available_for_adoption, pet.pet_type_id])
+        console.log(idArr.rows);
+      })
+     pool.end();
     } catch(error) {
       console.log(error);
+      pool.end();
     }
+
+
   }
 }
 
--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,7 +1,20 @@
+DROP TABLE IF EXISTS adoptable_pets;
 DROP TABLE IF EXISTS pet_types;
+
 CREATE TABLE pet_types (
   id SERIAL PRIMARY KEY,
   type VARCHAR(255) NOT NULL,
   img_url VARCHAR(255) NOT NULL,
   description VARCHAR(255)
+);
+
+CREATE TABLE adoptable_pets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  age INTEGER,
+  vaccination_status BOOLEAN DEFAULT FALSE,
+  adoption_story VARCHAR(255) NOT NULL,
+  available_for_adoption BOOLEAN DEFAULT TRUE,
+  pet_type_id INTEGER REFERENCES pet_types(id)
 );
