ae41af230b5caedc5028069ad0546592f178ca97
Jack Mason <jack.mason1@gmail.com>
2021-04-20 17:28:06 -0400

    Adoptable pets story

---
+++ client/components/AdoptablePetsOfAType.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react'
+
+import PetTile from "./PetTile.js";
+
+const AdoptablePetsOfAType = (props) => {
+  const typeName = props.match.params.type;
+  //State is a type of animal with an array of that type
+  const [petType, setPetType] = useState({
+    adoptablePets: []
+  });
+
+  const getData = async () => {
+    try {
+      const response = await fetch(`/api/v1/petTypes/pets/${typeName}`);
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`;
+        const error = new Error(errorMessage);
+        throw (error);
+      }
+      const responseBody = await response.json();
+      console.log(responseBody);
+      setPetType(responseBody.petType);
+    } catch (error) {
+      console.error(`error: ${error.message}`);
+    }
+  }
+
+  const pets = petType.adoptablePets.map( pet => {
+    return <PetTile
+      key = {pet.id}
+      pet = {pet}
+      type = {petType.type}
+      />
+  })
+
+  useEffect( () => {
+    getData();
+  }, [])
+
+
+  return (
+    <div>
+      <h1>Adoptable {petType.type}</h1>
+      {pets}
+    </div>
+  )
+
+
+
+}
+
+export default AdoptablePetsOfAType
---
+++ client/components/PetTile.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+
+const PetTile = (props) => {
+  const { id, name, imageUrl, age, vaccinationStatus } = props.pet;
+  return (
+    <div >
+      <Link to={`/pets/${props.type}/${id}`}>{name}</Link>
+      <Link to={`/pets/${props.type}/${id}`} >
+        <img src={imageUrl} alt={props.type} />
+      </Link>
+      <p>Age: {age}</p>
+      <p>Vaccination Status: {vaccinationStatus? 'YES' : 'NO'}</p>
+    </div>
+  )
+}
+
+export default PetTile
